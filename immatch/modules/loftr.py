777451183905daaefd8ef9df7bd7ad4e9128686a
Georg Boekman <georgbokman@outlook.com>
2022-03-30 11:40:56 +0200

    Allow for specifying fixed aspect ratio

--- immatch/modules/loftr.py
+++ immatch/modules/loftr.py
@@ -14,6 +14,7 @@ class LoFTR(Matching):
             args = Namespace(**args)
 
         self.imsize = args.imsize        
+        self.aspect_ratio = args.aspect_ratio
         self.match_threshold = args.match_threshold
         self.no_match_upscale = args.no_match_upscale
 
@@ -34,7 +35,7 @@ class LoFTR(Matching):
         
     def load_im(self, im_path):
         return load_gray_scale_tensor_cv(
-            im_path, self.device, imsize=self.imsize, dfactor=8
+            im_path, self.device, imsize=self.imsize, aspect_ratio=self.aspect_ratio, dfactor=8
         )
 
     def match_inputs_(self, gray1, gray2):
