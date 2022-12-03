37808d1e2cf09d98e7d58e09e3e7dc3d944eee8c
Qunjie Zhou <jennyzqj00@gmail.com>
2022-12-03 20:02:32 +0100

    Upload robotcar parsed query list (#40)

--- data/download.sh
+++ data/download.sh
@@ -2,3 +2,10 @@
 gdown --id 1T2c3DHJlTdRpo_Y3Ad4AH8p4tS7cOyjP
 unzip pairs.zip
 rm pairs.zip
+
+https://drive.google.com/file/d/13vfAKPG5lylXvr86gVW4WpDWoSOgidZ3/view?usp=share_link
+
+# Download HLoc robot car query list with intrinsics
+# File name: robotcar_season_queries_list_with_intrinsics.zip
+# After download + unzip, place it under:  repo_root/data/datasets/RobotCar/queries
+gdown --id 13vfAKPG5lylXvr86gVW4WpDWoSOgidZ3
