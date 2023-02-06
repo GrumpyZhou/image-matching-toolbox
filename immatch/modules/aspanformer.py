da0137583e1e99e740ea6ac178ea6365d2bc2ee7
Qunjie Zhou <jennyzqj00@gmail.com>
2023-02-06 02:39:22 +0100

    Replicate aspanformer data loading for reproducibility

--- immatch/modules/aspanformer.py
+++ immatch/modules/aspanformer.py
@@ -2,6 +2,7 @@ from argparse import Namespace
 import torch
 import numpy as np
 import cv2
+import torch.nn.functional as F
 
 from third_party.aspanformer.src.ASpanFormer.aspanformer import ASpanFormer as ASpanFormer_
 from third_party.aspanformer.src.config.default import get_cfg_defaults
@@ -9,6 +10,8 @@ from third_party.aspanformer.src.utils.misc import lower_config
 
 from .base import Matching
 from immatch.utils.data_io import load_gray_scale_tensor_cv
+from third_party.aspanformer.src.utils.dataset import read_megadepth_gray
+
 
 class ASpanFormer(Matching):
     def __init__(self, args):
@@ -16,14 +19,25 @@ class ASpanFormer(Matching):
         if type(args) == dict:
             args = Namespace(**args)
 
-        self.imsize = args.imsize        
+        self.imsize = args.imsize
         self.match_threshold = args.match_threshold
         self.no_match_upscale = args.no_match_upscale
+        self.online_resize = args.online_resize
+        self.im_padding = False
+        self.coarse_scale = args.coarse_scale
 
         # Load model
-        conf = lower_config(get_cfg_defaults().ASPAN)
+        config = get_cfg_defaults()
+        conf = lower_config(config)['aspan']
         conf['coarse']['train_res'] = args.train_res
+        conf['coarse']['test_res'] = args.test_res
+        conf['coarse']['coarsest_level'] = args.coarsest_level
+        conf['match_coarse']['border_rm'] = args.border_rm
         conf['match_coarse']['thr'] = args.match_threshold
+
+        if args.test_res:
+            self.imsize = args.test_res[0]
+            self.im_padding = args.test_res[0] == args.test_res[1]
         self.model = ASpanFormer_(config=conf)
         ckpt_dict = torch.load(args.ckpt)
         self.model.load_state_dict(ckpt_dict['state_dict'], strict=False)
@@ -34,16 +48,35 @@ class ASpanFormer(Matching):
         self.name = f'ASpanFormer_{self.ckpt_name}'        
         if self.no_match_upscale:
             self.name += '_noms'
-        print(f'Initialize {self.name}')
+        print(f'Initialize {self.name} {args} ')
 
     def load_im(self, im_path):
         return load_gray_scale_tensor_cv(
-            im_path, self.device, imsize=self.imsize, value_to_scale=max,
+            im_path,
+            self.device,
+            dfactor=8,
+            imsize=self.imsize,
+            value_to_scale=max,
+            pad2sqr=self.im_padding
         )
 
-    def match_inputs_(self, gray1, gray2):
-        batch = {'image0': gray1, 'image1': gray2}
-        self.model(batch, online_resize=True)
+    def match_inputs_(self, gray1, gray2, mask1=None, mask2=None):
+        batch = {
+            'image0': gray1, 'image1': gray2
+        }
+        if mask1 is not None and self.coarse_scale:
+            [ts_mask_1, ts_mask_2] = F.interpolate(
+                torch.stack([mask1, mask2], dim=0)[None].float(),
+                scale_factor=self.coarse_scale,
+                mode='nearest',
+                recompute_scale_factor=False
+            )[0].bool().to(self.device)
+            batch.update({'mask0': ts_mask_1.unsqueeze(0), 'mask1': ts_mask_2.unsqueeze(0)})
+
+        # Forward pass
+        self.model(batch, online_resize=self.online_resize)
+
+        # Output parsing
         kpts1 = batch['mkpts0_f'].cpu().numpy()
         kpts2 = batch['mkpts1_f'].cpu().numpy()
         scores = batch['mconf'].cpu().numpy()
@@ -51,10 +84,13 @@ class ASpanFormer(Matching):
         return matches, kpts1, kpts2, scores
 
     def match_pairs(self, im1_path, im2_path):
-        gray1, sc1 = self.load_im(im1_path)
-        gray2, sc2 = self.load_im(im2_path)
+        gray1, sc1, mask1 = self.load_im(im1_path)
+        gray2, sc2, mask2 = self.load_im(im2_path)
         upscale = np.array([sc1 + sc2])
-        matches, kpts1, kpts2, scores = self.match_inputs_(gray1, gray2)
+        matches, kpts1, kpts2, scores = self.match_inputs_(
+            gray1, gray2, mask1, mask2
+        )
+
         if self.no_match_upscale:
             return matches, kpts1, kpts2, scores, upscale.squeeze(0)
 
--- immatch/utils/data_io.py
+++ immatch/utils/data_io.py
@@ -1,7 +1,10 @@
 from PIL import Image
 import cv2
 import numpy as np
+
+import torch
 import torchvision.transforms as transforms
+
 
 def lprint(ms, log=None):
     '''Print message on console and in a log file'''
@@ -10,15 +13,18 @@ def lprint(ms, log=None):
         log.write(ms+'\n')
         log.flush()
 
-def resize_im(wo, ho, imsize=None, dfactor=1, value_to_scale=max):
+def resize_im(wo, ho, imsize=None, dfactor=1, value_to_scale=max, enforce=False):
     wt, ht = wo, ho
-    if imsize and value_to_scale(wo, ho) > imsize and imsize > 0:
+
+    # Resize only if the image is too big
+    resize = imsize and value_to_scale(wo, ho) > imsize and imsize > 0
+    if resize or enforce:
         scale = imsize / value_to_scale(wo, ho)
         ht, wt = int(round(ho * scale)), int(round(wo * scale))
 
     # Make sure new sizes are divisible by the given factor
     wt, ht = map(lambda x: int(x // dfactor * dfactor), [wt, ht])
-    scale = (wo / wt, ho / ht)
+    scale = [wo / wt, ho / ht]
     return wt, ht, scale
 
 def read_im(im_path, imsize=None, dfactor=1):
@@ -41,16 +47,43 @@ def load_gray_scale_tensor(im_path, device, imsize=None, dfactor=1):
     gray = transforms.functional.to_tensor(gray).unsqueeze(0).to(device)
     return gray, scale
 
-def load_gray_scale_tensor_cv(im_path, device, imsize=None, value_to_scale=min, dfactor=1):
-    # Used for LoFTR
+def load_gray_scale_tensor_cv(im_path, device, imsize=None, value_to_scale=min, dfactor=1, pad2sqr=False):
+    '''Image loading function applicable for LoFTR & Aspanformer. '''
+
     im = cv2.imread(im_path, cv2.IMREAD_GRAYSCALE)
     ho, wo = im.shape
     wt, ht, scale = resize_im(
-        wo, ho, imsize=imsize, dfactor=dfactor, value_to_scale=value_to_scale
+        wo, ho, imsize=imsize, dfactor=dfactor,
+        value_to_scale=value_to_scale,
+        enforce=pad2sqr
     )
     im = cv2.resize(im, (wt, ht))
+    mask = None
+    if pad2sqr and (wt != ht):
+        # Padding to square image
+        im, mask = pad_bottom_right(im, max(wt, ht), ret_mask=True)
+        mask = torch.from_numpy(mask).to(device)
     im = transforms.functional.to_tensor(im).unsqueeze(0).to(device)
-    return im, scale
+    return im, scale, mask
+
+def pad_bottom_right(inp, pad_size, ret_mask=False):
+    assert isinstance(pad_size, int) and pad_size >= max(inp.shape[-2:]), f"{pad_size} < {max(inp.shape[-2:])}"
+    mask = None
+    if inp.ndim == 2:
+        padded = np.zeros((pad_size, pad_size), dtype=inp.dtype)
+        padded[:inp.shape[0], :inp.shape[1]] = inp
+        if ret_mask:
+            mask = np.zeros((pad_size, pad_size), dtype=bool)
+            mask[:inp.shape[0], :inp.shape[1]] = True
+    elif inp.ndim == 3:
+        padded = np.zeros((inp.shape[0], pad_size, pad_size), dtype=inp.dtype)
+        padded[:, :inp.shape[1], :inp.shape[2]] = inp
+        if ret_mask:
+            mask = np.zeros((inp.shape[0], pad_size, pad_size), dtype=bool)
+            mask[:, :inp.shape[1], :inp.shape[2]] = True
+    else:
+        raise NotImplementedError()
+    return padded, mask
 
 def load_im_tensor(im_path, device, imsize=None, normalize=True,
                    with_gray=False, raw_gray=False, dfactor=1):
