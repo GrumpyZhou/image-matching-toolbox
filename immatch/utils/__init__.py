a6ed442d93b74afa787533f65d05f575516a408d
Qunjie Zhou <jennyzqj00@gmail.com>
2023-05-15 11:29:31 +0200

    Add a helping func for eval

--- immatch/utils/__init__.py
+++ immatch/utils/__init__.py
@@ -1,4 +1,13 @@
+import torch
+
 from third_party.patch2pix.utils.common.plotting import plot_matches
+
 
 def model_params(model):
     return sum(param.numel() for param in model.parameters()) / 1e6
+
+def dict_to_deivce(batch, device):
+    for k, v in batch.items():
+        if isinstance(v, torch.Tensor):
+            batch[k] = v.to(device)
+    return batch
