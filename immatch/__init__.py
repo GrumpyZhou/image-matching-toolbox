cb938e9a743f3a78ec81b27e7b3a1163aaa40288
ga1i13o <48190199+ga1i13o@users.noreply.github.com>
2024-03-05 20:14:49 +0100

    cleanups

--- immatch/__init__.py
+++ immatch/__init__.py
@@ -1,4 +1,3 @@
-from .modules.caps import CAPS
 from .modules.superpoint import SuperPoint
 from .modules.superglue import SuperGlue
 from .modules.d2net import D2Net
@@ -9,23 +8,3 @@ from .modules.sift import SIFT
 from .modules.dogaffnethardnet import DogAffNetHardNet
 from .modules.cotr import COTR
 
-try:
-    from .modules.aspanformer import ASpanFormer
-except ImportError as e:
-    print(f"Can not import ASpanFormer: {e}")
-    pass
-
-try:
-    import MinkowskiEngine
-    import sys
-    from pathlib import Path
-    
-    # To prevent naming conflict as D2Net also has module called lib   
-    d2net_path = Path(__file__).parent / 'modules/../../third_party/d2net'
-    sys.path.remove(str(d2net_path))
-    
-    from .modules.sparsencnet import SparseNCNet     
-    use_sparsencnet = True
-except ImportError as e:
-    print(f"Can not import sparsencnet: {e}")
-    pass    
